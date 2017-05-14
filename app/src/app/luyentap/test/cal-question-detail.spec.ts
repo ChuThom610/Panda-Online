dbf1716bb763bc35dad02a8258d16c7d96d2f858
ChuThom610 <chuthom97@gmail.com>
2017-05-14 22:06:19 +0700

    Delete cal-question-detail.spec.ts

--- app/src/app/luyentap/test/cal-question-detail.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CalQuestionDetailComponent } from 'app/luyentap/cal-question-detail.component';
-
-describe(`Component: CalQuestionDetailComponent`, function(){
-    it(`should add a star if input is 7 `, ()=>{
-       // const result = checkAns(7);
-        expect(3 + 4 ).toEqual(7);
-    });
-});
